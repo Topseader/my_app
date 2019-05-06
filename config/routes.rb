c33719080e5c501f146b43e1c1734db6dc3b701b
Mikhail Kharitonov <mh@megaperfume.ru>
2019-05-06 12:47:31 +0300

    add main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'welcome/index' # объявление страницы
+
+  root 'welcome#index' # задание страницы как главной
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
